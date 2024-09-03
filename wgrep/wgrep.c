b3cf62fceea05a9e32ff581de5e25fba676765c9
Every <christiansantoslima21@gmail.com>
2024-09-03 17:05:18 -0300

    feat: finish wgrep implementation

--- wgrep/wgrep.c
+++ wgrep/wgrep.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 100
 
 int main(int argc, char** argv) {
   FILE *fp;
   size_t size = 0;
   char *buffer = NULL;
+  char buf[BUFFER_SIZE];
+  char *result;
   if (argc < 2) {
-    printf("wgrep: searchterm [file ...]");
+    printf("wgrep: searchterm [file ...]\n");
     exit(1);
+  }
+
+  if (argc == 2) {
+    while((result = fgets(buf, BUFFER_SIZE, stdin)) != NULL)
+      if (strstr(result, argv[1]) != NULL)
+        printf("%s", result);
   }
 
   for (int i = 2; i < argc; i++) {
