f99addf9c6b8bda58d5b3d529da5dcc7e9226f69
Every <christiansantoslima21@gmail.com>
2024-09-04 15:35:53 -0300

    chore: fixed wrong typo

--- wzip/wzip.c
+++ wzip/wzip.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
   for (int i = 1; i < argc; ++i) {
     fp = fopen(argv[i], "r");
     if (fp == NULL) {
-      printf("wcat: cannot open file\n");
+      printf("wzip: cannot open file\n");
       exit(1);
     }
 
