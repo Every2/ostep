48f4fbe113113628c8d22a1e2a3fe0f2c874f284
Every <christiansantoslima21@gmail.com>
2024-09-04 15:08:58 -0300

    feat: implement wzip exercise

---
+++ wzip/wzip.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  FILE *fp;
+  char buffer[2] = {0};
+  char pattern[2] = {0};
+  int j = 0;
+
+  if (argc < 2) {
+    printf("wzip: file1 [file2 ...]\n");
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    fp = fopen(argv[i], "r");
+    if (fp == NULL) {
+      printf("wcat: cannot open file\n");
+      exit(1);
+    }
+
+    while (fread(buffer, 1, 1, fp)) {
+      if (strcmp(buffer, pattern) == 0)
+        j++;
+      else {
+        if (pattern[0] != '\0') {
+          fwrite(&j, 4, 1, stdout);
+          fwrite(pattern, 1, 1, stdout);
+        }
+        j = 1;
+        strcpy(pattern, buffer);
+      }
+  }
+  fclose(fp);
+ }
+ fwrite(&j, 4, 1, stdout);
+ fwrite(pattern, 1, 1, stdout);
+ return 0;
+}
